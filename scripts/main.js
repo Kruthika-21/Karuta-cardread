33901424530150672ba310fdd10168a52189837e
KRUTHIKA <pinnadakruthika001@gmail.com>
2022-07-05 19:00:09 +0530

    my changes

---
+++ scripts/main.js
@@ -0,0 +1,203 @@
+var images = ["http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/2 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/2 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/2 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/2 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/3 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/3 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/3 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/3 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/4 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/4 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/4 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/4 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/5 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/5 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/5 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/5 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/6 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/6 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/6 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/6 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/7 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/7 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/7 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/7 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/8 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/8 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/8 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/8 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/9 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/9 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/9 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/9 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/10 of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/10 of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/10 of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/10 of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/ace of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/ace of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/ace of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/ace of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/jack of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/jack of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/jack of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/jack of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/king of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/king of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/king of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/king of spades.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/queen of clubs.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/queen of diamonds.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/queen of hearts.png",
+    "http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/queen of spades.png",
+];
+var imagessound = ["2 of clubs",
+    "2 of diamonds",
+    "2 of hearts",
+    "2 of spades",
+    "3 of clubs",
+    "3 of diamonds",
+    "3 of hearts",
+    "3 of spades",
+    "4 of clubs",
+    "4 of diamonds",
+    "4 of hearts",
+    "4 of spades",
+    "5 of clubs",
+    "5 of diamonds",
+    "5 of hearts",
+    "5 of spades",
+    "6 of clubs",
+    "6 of diamonds",
+    "6 of hearts",
+    "6 of spades",
+    "7 of clubs",
+    "7 of diamonds",
+    "7 of hearts",
+    "7 of spades",
+    "8 of clubs",
+    "8 of diamonds",
+    "8 of hearts",
+    "8 of spades",
+    "9 of clubs",
+    "9 of diamonds",
+    "9 of hearts",
+    "9 of spades",
+    "10 of clubs",
+    "10 of diamonds",
+    "10 of hearts",
+    "10 of spades",
+    "ace of clubs",
+    "ace of diamonds",
+    "ace of hearts",
+    "ace of spades",
+    "jack of clubs",
+    "jack of diamonds",
+    "jack of hearts",
+    "jack of spades",
+    "king of clubs",
+    "king of diamonds",
+    "king of hearts",
+    "king of spades",
+    "queen of clubs",
+    "queen of diamonds",
+    "queen of hearts",
+    "queen of spades",];
+
+var curImageIndex = 0; // 0 is displayed by default
+var intervalId; //Remember the ID of the interval so we can stop it later.
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+function picturechange()
+{
+    
+    var imageseq = document.getElementById("Startimag")
+    
+    imageseq.src="http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/cardgif.gif"
+    
+}
+function startImageCycle(){
+    let len=images.length;
+    var imageseq = document.getElementById("Startimag")
+    let cardNumber =  Math.floor(len * Math.random());
+    curImageIndex++;
+    if (curImageIndex>52)
+    {
+        imageseq.src="http://127.0.0.1:5502/PNG-cards-1.3/PNG-cards-1.3/black joker.png"
+        speech.text="End of the game"
+        window.speechSynthesis.speak(speech)
+        sleep(2000);
+    setTimeout("location.reload(true);",1500)
+    }
+    else{
+        speech.text=imagessound[cardNumber]
+        imageseq.src=images[cardNumber]
+        window.speechSynthesis.speak(speech);
+        const index = images.indexOf(images[cardNumber]);
+        if (index > -1) { // only splice array when item is found
+            images.splice(index, 1);
+            imagessound.splice(index, 1); // 2nd parameter means remove one item only
+          }
+
+    }
+    function sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+          currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+      }
+          
+    
+
+}
+
+
+
+/*function start() {
+    let button1 = document.getElementById("button1");
+    button1.onclick = toggleContent;
+}
+
+function toggleContent() {
+    let number = +document.getElementById('number').value; // take value as a number
+    let liquid = document.getElementById('Bar');
+
+    if (isNaN(number) || number < 0) { // move exit condition to top and exit early
+        alert("Invalid Entry. Enter a Number.")
+        return;
+    }
+
+    if (number <= 20) { // condition without parseint 
+        liquid.src = 'assets/Soda.png';
+        liquid.alt = 'Spicy water';
+    } else { // no need for another check
+        liquid.src = 'assets/Beer.png';
+        liquid.alt = 'Angry Juice';
+    }
+}
+
+window.onload = start;*/
+/*function start() {
+    let button1 = document.getElementById("button1");
+    button1.onclick = toggleContent;
+}
+
+function toggleContent() {
+    let number = 1 + Math.floor(52 * Math.random()); // take value as a number
+    let liquid = document.getElementById('Bar');
+
+    if (isNaN(number) || number < 0) { // move exit condition to top and exit early
+        alert("Invalid Entry. Enter a Number.")
+        return;
+    }
+
+    if (number <= 20) { // condition without parseint 
+        liquid.src = 'assets/Soda.png';
+        liquid.alt = 'Spicy water';
+    } else { // no need for another check
+        liquid.src = 'assets/Beer.png';
+        liquid.alt = 'Angry Juice';
+    }
+}
+window.onload = start;*/   
